f01e136cf2ccf2c7045f01088fef1d8db84c3779
박설아 <112845496+Parksealah@users.noreply.github.com>
2022-10-31 10:40:55 +0900

    finished chapter 08

---
+++ c_switch.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+//제어문
+//1. if~elso if~elso
+//2.switch ~ case ~ default
+/**
+ * 여러개의 실수 중 조건에 해당하는 하나만 골라서 실행
+ * 
+ * -규칙
+ * 1) 조건식은 점수식만 시용
+ * 2) 기본젓으로 case는 break문을 포함 (무조건 아님)
+ * 3) break가 없는 경우 break만나기 전까지 모든 case 실행
+ * 4) default는 조건에 해당하는 case가 없는 경우 실행
+ * 5) default는 생략가능 (if문 의 elso와 비숫)
+ * 6) default는 어디든 위치해도 상관없없음, 대부분 맨 하단
+ * 7) if문보다 switch문이 실행 속도 빠름
+*/
+
+int main (void){
+    int rank = 2, m = 0 ;
+
+    switch (rank) {
+        case 1:
+            m = 300;
+            break;
+        case 2:
+            m = 200;
+            break;
+        case 3:
+            m = 100;
+            break;
+        default:
+            m = 10;
+            break;
+    }
+   
+}
